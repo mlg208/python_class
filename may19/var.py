5fdc31049e00edf46585898bd3bdb4d9a201f859
mlg208 <daniils0811@mail.ru>
2023-05-23 21:52:54 +0600

    var.py
    changes

--- may19/var.py
+++ may19/var.py
@@ -1,5 +1,6 @@
 user_input = input("ввести число")
 user_input = input("ввести число")
-user_int = int(user_input)
-
+user_int1 = int(user_input)
+user_int2 = int(user_input)
+result = (user_int1 + user_int2) /2
 print("результат",result)
